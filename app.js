6e04cd4a034eddb53ab233d1ac0ecf88d3ad4c8f
KlaudiaKowalska <150347636+KlaudiaKowalska@users.noreply.github.com>
2024-09-10 17:35:53 +0200

    1735

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const connectDB = require("./db");
+const path = require("path");
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
@@ -14,14 +15,19 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 
+// Obsługa plików statycznych
+app.use("/avatars", express.static("public/avatars"));
+
 // Użyj routerów
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
+// Obsługa błędów 404
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
+// Obsługa błędów serwera
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
